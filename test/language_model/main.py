1d686db327bb646c26871dcbb86b05c45170bb0b
liyzcj <liyz0912@gmail.com>
2019-04-09 09:18:24 +0800

    save and restore model

--- test/language_model/main.py
+++ test/language_model/main.py
@@ -18,15 +18,15 @@ class Params:
     keep_prob = 1.0
     num_layers = 2
     grad_clip = 5
-    num_epoch = 10
+    num_epoch = 1
     learninig_rate = 1.0
     decay_epoch = 4
     lr_decay = 0.5
     embedding_size = 200
     vocab_size = 10000
 
-
-
+restore_path = "test/language_model/save/last_weights"
+save_path = "test\\language_model\\save"
 data_path = "test/data/"
 train_path = os.path.join(data_path, "ptb.train.txt")
 valid_path = os.path.join(data_path, "ptb.valid.txt")
@@ -63,19 +63,31 @@ with tf.name_scope("Test"):
 
 init = tf.group(*[tf.global_variables_initializer(), tf.tables_initializer()])
 
-# saver = tf.train.Saver()
+saver = tf.train.Saver()
 # sv = tf.train.Supervisor(logdir="test/language_model/logs/", init_op=init)
 # saver=sv.saver
 
 
 # with sv.managed_session(config=config) as sess:
-with tf.Session() as sess:
+with tf.Session(config=config) as sess:
     sess.run(init)
     sess.run(train_input.init_op)
     sess.run(valid_input.init_op)
     sess.run(test_input.init_op)
 
-    for i in range(train_params.num_epoch):
+    bae = 0
+    # Reload weights if exits
+    if os.path.exists(restore_path):
+        print("Restoring parameters from {}".format(restore_path))
+        if os.path.isdir(restore_path):
+            restore_path = tf.train.latest_checkpoint(restore_path)
+            # Begin at epoch
+            bae = int(restore_path.split('-')[-1])
+        saver.restore(sess, restore_path)
+
+    # end of epoch 
+    eoe = bae + train_params.num_epoch
+    for i in range(bae, eoe):
         lr_decay = train_params.lr_decay ** max(i+1-train_params.decay_epoch, 0.0)
         train_model.assign_lr(sess,train_params.learninig_rate * lr_decay)
         print("Epoch: %d Learning rate: %.3f" % (i + 1, sess.run(train_model.lr)))
@@ -83,8 +95,11 @@ with tf.Session() as sess:
         print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_perplexity))
         valid_perplexity = valid_model.run_one_epoch(sess)
         print("Epoch: %d Valid Perplexity: %.3f" % (i + 1, valid_perplexity))
-    test_perplexity = test_model.run_one_epoch(sess)
-    print("Test Perplexity: %.3f" % test_perplexity)
+        ## Save model
+        path = os.path.join(save_path, 'last_weights', 'after-epoch')
+        saver.save(sess, path, global_step=i+1)
+    # test_perplexity = test_model.run_one_epoch(sess)
+    # print("Test Perplexity: %.3f" % test_perplexity)
 
 
 # train_input = PTBInputs(train_path, train_params)
